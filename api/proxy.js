f49cb116d413bcb72610d5937230f73e93f6d3f9
pugna <674408526@qq.com>
2021-12-07 10:56:47 +0800

    vercel部署时的跨域问题2

---
+++ api/proxy.js
@@ -0,0 +1,17 @@
+// 该服务为vercel serve跨域处理
+const { createProxyMiddleware } = require('http-proxy-middleware')
+module.exports = (req,res) => {
+  let target = ''
+  // 代理目标地址
+  if(req.url.startsWith('/api')){
+      target = 'http://152.136.185.210:7878/api/hy66/'
+  }
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/': '/'
+    }
+  })(req,res)
+}
---
+++ src/views/home/childcpns/HomeFloordata.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="floordata">
+    <div v-for="item in floordata" :key="item.index">
+      <!-- 标题 -->
+      <div class="floor_title">
+        <img :src="item.floor_title.image_src" alt="">
+      </div>
+      <!-- 内容 -->
+      <div class="floor_list">
+        <div v-for="items in item.product_list" :key="items.index">
+          <img :src="items.image_src" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeFloordata',
+  props: {
+      floordata: {
+        type: Array,
+        default(){
+          return []
+        }   
+      }
+  }
+}
+</script>
+
+<style>
+  #floordata {
+    margin: 20px 10px;
+  }
+  .floor_list div{
+    float: left;
+    width: 33.33%;
+    padding: 2px;
+  }
+  .floor_list div:nth-child(n+2){
+      height: 97px;
+  }
+  .floor_list img {
+    width: 100%;
+    height: 100%;
+  }
+</style>
