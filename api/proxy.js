48d3f979aee5e8937619339b5417bbf08b2f5fb1
pugna <674408526@qq.com>
2021-12-07 12:25:34 +0800

    vercel部署跨域5

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ module.exports = (req,res) => {
   let target = ''
   // 代理目标地址
   if(req.url.startsWith('/api')){
-      target = 'http://152.136.185.210:7878/api/'
+      target = 'http://152.136.185.210:7878'
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
